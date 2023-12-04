d4f0d13dd0aa4c12f9a97a6df188cb1bc4fc44bb
Akara4ok <vladshpilka86@gmail.com>
2023-12-04 23:27:14 +0200

    add calculating avg load and probability

--- main.cpp
+++ main.cpp
@@ -7,9 +7,28 @@
 int main() {
     srand(time(NULL));
     Logger logger("Logs.csv");
-    Model model = ModelFactory::createModel1();
+//    int num = 100;
+//    double avgLoad{};
+//    double avgProb{};
 
-    model.simulate(100000);
+//    for (int i = 0; i < num; ++i) {
+        Model model = ModelFactory::createModel1();
+
+//        model.getElementByName("Machine Second Phase")->setSummaryFunction([&avgLoad](Element* element){
+//            auto* casted_el = dynamic_cast<MultiChannelProcess*>(element);
+//            avgLoad += casted_el->getAverageLoad(1);
+//        });
+//        model.setSummaryFunction([&avgProb](Model* model){
+//            int defected = model->getElementByName("Defected")->getProceed();
+//            int proceed11 = model->getElementByName("First Machine First Phase")->getProceed();
+//            int proceed12 = model->getElementByName("Second Machine First Phase")->getProceed();
+//            avgProb += (double)defected / (proceed11 + proceed12);
+//        });
+        model.simulate(100000);
+//    }
+
+//    std::cout << "Avg load: " << avgLoad / num << "\n";
+//    std::cout << "Avg prob: " << avgProb / num << "\n";
 
     Logger::saveLogFile();
 }
